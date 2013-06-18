dd60e64aac15aa1a7ee35c1830cb79d044b0633e
Ben Israeli <jisraeli@gmail.com>
2013-06-18 12:21:44 -0400

    first commit

---
+++ madlibs.rb
@@ -0,0 +1,32 @@
+#This line will output
+puts "Give me a noun"
+
+#This line asks for input
+first_noun = gets.chomp
+
+#This line will output
+puts "Give me an adverb"
+#This line asks for input
+first_adverb = gets.chomp
+
+#This line will output
+puts "Give me a present tense verb suckah!"
+#This line asks for input
+first_verb = gets.chomp
+
+#This line will output
+puts "Now hit me with an adjective baby baby!"
+#This line asks for input
+first_adjective = gets.chomp
+
+#This line will output
+puts "Gimme a gerund killah. If you don't know what a gerund is ask your auntie"
+#This lines ask for input
+first_gerund = gets.chomp
+
+puts "Eric pulled out his #{first_noun} " +
+      "and looked at it #{first_adverb}. " +
+      "His mother, a dentist, had raised him to #{first_verb} " +
+      "from a young age, and, although he had finally become #{first_adjective}, " +
+      "after years of waiting to hit puberty, " +
+      "he still found it difficult to commit to #{first_gerund}."
